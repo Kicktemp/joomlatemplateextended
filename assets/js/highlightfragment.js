b3f0b8fc778e6174ee960d7f5c8f2b5b615426cd
Niels Nübel <n.nuebel@nn-medienagentur.de>
2016-09-14 22:21:37 +0200

    Update

--- assets/js/highlightfragment.js
+++ assets/js/highlightfragment.js
@@ -18,6 +18,19 @@
                     element.innerHTML = element.innerHTML.replace(search, replace);
                 }
             }
+
+            // Now escape html unless prevented by author
+            if( element.hasAttribute( 'data-inline' )) {
+                var regexp = /&lt;mark class="inline/g;
+
+                while ((match = regexp.exec(element.innerHTML)) != null) {
+                    var replace = '<span' + element.innerHTML.substring(match.index+8, match.index + element.innerHTML.substring(match.index).indexOf('&gt;')) + '>';
+                    replace = replace + element.innerHTML.substring(match.index + element.innerHTML.substring(match.index).indexOf('&gt;')+4, match.index + element.innerHTML.substring(match.index).indexOf('&lt;/mark&gt;')) + '</span>';
+                    var search = element.innerHTML.substring(match.index, match.index + element.innerHTML.substring(match.index).indexOf('&lt;/mark&gt;')+13);
+
+                    element.innerHTML = element.innerHTML.replace(search, replace);
+                }
+            }
         }
     }
 })();
